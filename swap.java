2a2a360fae4da7d5486b569c72f642cd3324ee15
Keerthika <dkeerthika1808@gmail.com>
2026-01-11 19:38:16 +0530

    first commit
    Swaping the numbers without Temp

---
+++ swap.java
@@ -0,0 +1,14 @@
+package BasicMath;
+
+public class swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b = 5;
+        System.out.println("Before swapping: " + a +" " + b);
+
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("after swapping: " + a+" " + b);
+    }
+}
